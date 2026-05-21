21dc4c15772836c25ab4d610ca3a0739d6c59f80
Dima <dima@MacBook-Air-Dima.local>
2026-05-21 12:25:34 +0300

    Add Raduga camp landing page

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/raduga',
+        destination: '/raduga.html',
+      },
+    ];
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
